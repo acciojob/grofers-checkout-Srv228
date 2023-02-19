f17ad6f8053f259d41ae21d4cc2a0b950d933a24
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-02-19 17:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll('.price');
+  let total = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    total += parseFloat(prices[i].textContent);
+  }
+
+  const table = document.querySelector('table');
+  const newRow = table.insertRow();
+  const newCell = newRow.insertCell();
+  newCell.colSpan = 2;
+  newCell.style.fontWeight = 'bold';
+  newCell.textContent = `Total price: $${total.toFixed(2)}`;
 };
 
-getSumBtn.addEventListener("click", getSum);
+getSumBtn.addEventListener('click', getSum);
 
+getSumBtn.addEventListener("click", getSum);
